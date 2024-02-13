f88d69bcd1ed85e0f794f92a0ed10d48308dc7af
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2024-02-14 09:26:00 +1300

    Working string checker for y/n and r/p/s

--- C_02_String_Checker_v3.py
+++ C_02_String_Checker_v3.py
@@ -1,6 +1,8 @@
 # Check that users have entered a valid
 # option based on a list
-def string_checker(question, valid_ans):
+def string_checker(question, valid_ans=['yes', 'no']):
+    error = f"Please enter a valid answer from the following list: {valid_ans}"
+
     while True:
 
         user_response = input(question).lower()
@@ -15,11 +17,18 @@ def string_checker(question, valid_ans):
             elif user_response == item[0]:
                 return item
 
+        # Print error if the user does not enter something that is valid
+        print(error)
+        print()
+
 
 # Main routine
 
-yes_no = ["yes", "no"]
 rps_list = ["rock", "paper", "scissors", "xxx"]
 
-want_instructions = string_checker("Do you want ot see the instructions? ",
-                                   yes_no)
+# Asks the user if they want to see the instructions
+want_instructions = string_checker("Do you want to see the instructions? ")
+print(f"You chose {want_instructions}")
+
+user_choice = string_checker("Rock, Paper or Scissors? ", rps_list)
+print(user_choice)
